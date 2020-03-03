aff1e31d7b46687dadfa95700e90c88eb437ae1f
Matthew.Hannay <matthew.hannay@datacom.com.au>
2020-03-03 16:25:56 +1100

    fix up @Bean

--- src/main/java/contact/hfttech/biz/contactinfo/ContactinfoApplication.java
+++ src/main/java/contact/hfttech/biz/contactinfo/ContactinfoApplication.java
@@ -1,8 +1,10 @@
 package contact.hfttech.biz.contactinfo;
 
+import contact.hfttech.biz.service.bo.ProcessContact;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -13,6 +15,11 @@ public class ContactinfoApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(ContactinfoApplication.class, args);
+	}
+
+	@Bean
+	public ProcessContact getProcessContact() {
+		return new ProcessContact();
 	}
 
 }
